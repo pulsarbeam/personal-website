4e31d12747a80ddc6808822e516835a8e2a2f813
Rene Groothuis <renegrooth@gmail.com>
2023-04-16 11:03:36 +1200

    intial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+  return <div className="bg-[#60657F] w-full h-screen"></div>
+}
+
+export default Home
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#60657F] text-slate-200">
+      <div></div>
+      {/* Menu */}
+
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!isOpen ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !isOpen
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#60657F] flex flex-col justify-center items-center'
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160] h-[60px] flex justify-between items-center  ml-[-75px] hover:ml-[-10px] duration-300 bg-slate-700 rounded-md">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              <span className="pr-2">GitHub</span>
+              <FaGithub size={30} className="ml-2" />
+            </a>
+          </li>
+          <li className="w-[160] h-[60px] flex justify-between items-center  ml-[-75px] hover:ml-[-10px] duration-300 bg-slate-700 rounded-md">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              <span className="pr-2">LinkedIn</span>
+              <FaLinkedin size={30} className="ml-2" />
+            </a>
+          </li>
+        </ul>
+      </div> */}
+    </div>
+  )
+}
+
+export default Navbar
