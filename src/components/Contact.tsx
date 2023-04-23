6808c1c3f1a80828a8451c280bee73735cc15cae
Rene Groothuis <renegrooth@gmail.com>
2023-04-23 17:19:59 +1200

    Added contact smooth scroll

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,47 @@
+import { Element } from 'react-scroll'
 
 const Contact = () => {
-  return(
-    <div className='w-full h-full bg-[#200130]'>
-
-    </div>
+  return (
+    <Element name="contact">
+      <div className="w-full h-full bg-[#200130] flex justify-center items-center p-4">
+        <form
+          method="POST"
+          action="https://getform.io/f/31cbc424-080f-4ccb-bd35-b23de9773adb"
+          className="flex flex-col max-w-[600px] w-full"
+        >
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+              Contact
+            </p>
+            <p className="text-gray-300 py-4">
+              // Submit the form below or shoot me an email -
+              renegrooth@gmail.com
+            </p>
+          </div>
+          <input
+            className="p-2 bg-[#ccd6f6]"
+            type="text"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            className="my-4 p-2 bg-[#ccd6f6]"
+            type="email"
+            placeholder="Email"
+            name="email"
+          />
+          <textarea
+            className="p-2 bg-[#ccd6f6]"
+            name="message"
+            rows={10}
+            placeholder="Message"
+          ></textarea>
+          <button className="text-white border-2 hover:bg-pink-600 px-4 py-3 my-8 mx-auto flex items-center rounded">
+            Let's Collaborate
+          </button>
+        </form>
+      </div>
+    </Element>
   )
 }
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,4 @@
+import { Element } from 'react-scroll'
 import Github from '../assets/github.png'
 import Javascript from '../assets/javascript.png'
 import Knex from '../assets/knex.png'
@@ -7,43 +8,45 @@ import Typescript from '../assets/typescript.png'
 
 const Skills = () => {
   return (
-    <div className="w-full h-screen bg-[#200130] text-gray-300">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
-            Skills
-          </p>
-          <p className="py-4">These are the technologies I've worked with</p>
-        </div>
+    <Element name="skills">
+      <div className="w-full h-screen bg-[#200130] text-gray-300">
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+          <div>
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+              Skills
+            </p>
+            <p className="py-4">These are the technologies I've worked with</p>
+          </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Javascript} alt="Javascript" />
-            <p className="my-4">Javascript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Typescript} alt="Typescript" />
-            <p className="my-4">Typescript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Knex} alt="Knex" />
-            <p className="my-4">Knex</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="Node" />
-            <p className="my-4">Node</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={React} alt="React" />
-            <p className="my-4">React</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Github} alt="Github" />
-            <p className="my-4">Github</p>
+          <div className="w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8">
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={Javascript} alt="Javascript" />
+              <p className="my-4">Javascript</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={Typescript} alt="Typescript" />
+              <p className="my-4">Typescript</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={Knex} alt="Knex" />
+              <p className="my-4">Knex</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={Node} alt="Node" />
+              <p className="my-4">Node</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={React} alt="React" />
+              <p className="my-4">React</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img className="w-20 mx-auto" src={Github} alt="Github" />
+              <p className="my-4">Github</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Element>
   )
 }
 
