e268f6841994d3fb318479f0b47ae8c0a68be8bc
Rene Groothuis <renegrooth@gmail.com>
2023-04-27 13:54:41 +1200

    added cv

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded-sm">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="/personal-website/public/Rene-CV.pdf"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
